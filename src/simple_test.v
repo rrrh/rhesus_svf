1682b3facfa004fdd55a6e7525a4c3cd7e31e935
Sven Huerlimann <sven.huerlimann@rocket-science.ch>
2026-08-17 20:51:02 +0000

    Update

--- src/simple_test.v
+++ src/simple_test.v
@@ -11,8 +11,8 @@ module tt_um_simple_test (
     input  wire       rst_n     
 `ifdef USE_POWER_PINS
     , // DO NOT FORGET THIS COMMA
-    inout  wire       VPWR,
-    inout  wire       VGND
+    inout  wire       vccd1,
+    inout  wire       vssd1
 `endif
 );
 
@@ -42,8 +42,8 @@ module tt_um_simple_test (
     (* keep = 1 *)
     r2r_dac_8bit u_dac (
 `ifdef USE_POWER_PINS
-        .vdd(VPWR),
-        .vss(VGND),
+        .vdd(vccd1),
+        .vss(vssd1),
 `endif
         .d0(ui_in[0]), .d1(ui_in[1]), .d2(ui_in[2]), .d3(ui_in[3]),
         .d4(ui_in[4]), .d5(ui_in[5]), .d6(ui_in[6]), .d7(ui_in[7]),
@@ -53,8 +53,8 @@ module tt_um_simple_test (
     (* keep = 1 *)
     r2r_dac_8bit u_ramp_dac (
 `ifdef USE_POWER_PINS
-        .vdd(VPWR),
-        .vss(VGND),
+        .vdd(vccd1),
+        .vss(vssd1),
 `endif
         .d0(ramp_cnt[0]), .d1(ramp_cnt[1]), .d2(ramp_cnt[2]), .d3(ramp_cnt[3]),
         .d4(ramp_cnt[4]), .d5(ramp_cnt[5]), .d6(ramp_cnt[6]), .d7(ramp_cnt[7]),
@@ -64,8 +64,8 @@ module tt_um_simple_test (
     (* keep = 1 *)
     pwm_comp u_comp (
 `ifdef USE_POWER_PINS
-        .vdd(VPWR),
-        .vss(VGND),
+        .vdd(vccd1),
+        .vss(vssd1),
 `endif
         .vinp(dac_to_comp),
         .vinn(ramp_to_comp),
