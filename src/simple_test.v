ccbf6242d4ed3c81d1b8e96ee1dce953f696a6fd
Sven Huerlimann <sven.huerlimann@rocket-science.ch>
2026-08-19 12:05:23 +0000

    Fix OpenLane 2 PDN variable (FP_PDN_MACRO_HOOKS) to finally drop power vias and clear LVS/DRC

--- src/simple_test.v
+++ src/simple_test.v
@@ -1,5 +1,4 @@
 `default_nettype none
-
 module tt_um_simple_test (
     input  wire [7:0] ui_in,    
     output wire [7:0] uo_out,   
@@ -15,11 +14,9 @@ module tt_um_simple_test (
     inout  wire       vssd1
 `endif
 );
-
     wire dac_to_comp;
     wire ramp_to_comp;
     wire comp_out;
-
     reg [7:0] ramp_cnt;
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) 
@@ -27,31 +24,38 @@ module tt_um_simple_test (
         else        
             ramp_cnt <= (ramp_cnt == 8'd254) ? 8'd0 : ramp_cnt + 8'd1;
     end
-
     assign uo_out[0]   = comp_out;
     assign uo_out[7:1] = ramp_cnt[7:1];
     assign uio_out     = ramp_cnt;
     assign uio_oe      = ~ramp_cnt;
-
     (* keep = 1 *)
     r2r_dac_8bit u_dac (
+`ifdef USE_POWER_PINS
+        .vdd(vccd1),
+        .vss(vssd1),
+`endif
         .d0(ui_in[0]), .d1(ui_in[1]), .d2(ui_in[2]), .d3(ui_in[3]),
         .d4(ui_in[4]), .d5(ui_in[5]), .d6(ui_in[6]), .d7(ui_in[7]),
         .vout(dac_to_comp)
     );
-
     (* keep = 1 *)
     r2r_dac_8bit u_ramp_dac (
+`ifdef USE_POWER_PINS
+        .vdd(vccd1),
+        .vss(vssd1),
+`endif
         .d0(ramp_cnt[0]), .d1(ramp_cnt[1]), .d2(ramp_cnt[2]), .d3(ramp_cnt[3]),
         .d4(ramp_cnt[4]), .d5(ramp_cnt[5]), .d6(ramp_cnt[6]), .d7(ramp_cnt[7]),
         .vout(ramp_to_comp)
     );
-
     (* keep = 1 *)
     pwm_comp u_comp (
+`ifdef USE_POWER_PINS
+        .vdd(vccd1),
+        .vss(vssd1),
+`endif
         .vinp(dac_to_comp),
         .vinn(ramp_to_comp),
         .out(comp_out)
     );
-
 endmodule
