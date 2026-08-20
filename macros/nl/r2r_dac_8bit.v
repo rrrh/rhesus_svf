56ba5546d2b3a26f8c54a0f7d46c4c4a4c9df7c2
Sven Huerlimann <sven.huerlimann@rocket-science.ch>
2026-08-20 15:06:57 +0000

    Route all macro power pins with TritonRoute to match schematic in LVS

--- macros/nl/r2r_dac_8bit.v
+++ macros/nl/r2r_dac_8bit.v
@@ -1,6 +1,6 @@
 (* blackbox *)
 module r2r_dac_8bit (
-    input vdd, input vss, output vout,
+    inout vdd, inout vss, output vout,
     input d0, input d1, input d2, input d3,
     input d4, input d5, input d6, input d7
 );
